3811745881bf193cc438412a07d23522f9bce47e
Ruben Maurer <rubenmaurer@live.de>
2018-01-09 13:15:43 +0100

    Update CommandListener.java
    Removed unused methods.

--- src/main/java/de/rubenmaurer/punk/core/irc/parser/CommandListener.java
+++ src/main/java/de/rubenmaurer/punk/core/irc/parser/CommandListener.java
@@ -1,17 +1,14 @@
 package de.rubenmaurer.punk.core.irc.parser;
 
 import akka.actor.ActorRef;
-import de.rubenmaurer.punk.IRCListener;
+import de.rubenmaurer.punk.IRCBaseListener;
 import de.rubenmaurer.punk.IRCParser;
+import de.rubenmaurer.punk.core.irc.messages.MessageBuilder;
 import de.rubenmaurer.punk.core.irc.messages.impl.Change;
 import de.rubenmaurer.punk.core.irc.messages.impl.Chat;
-import de.rubenmaurer.punk.core.irc.messages.MessageBuilder;
 import de.rubenmaurer.punk.util.Notification;
 import de.rubenmaurer.punk.util.Settings;
 import de.rubenmaurer.punk.util.Template;
-import org.antlr.v4.runtime.ParserRuleContext;
-import org.antlr.v4.runtime.tree.ErrorNode;
-import org.antlr.v4.runtime.tree.TerminalNode;
 import org.apache.commons.lang3.StringUtils;
 
 /**
@@ -21,7 +18,7 @@ import org.apache.commons.lang3.StringUtils;
  * @version 1.0
  * @since 1.0
  */
-public class CommandListener implements IRCListener {
+public class CommandListener extends IRCBaseListener {
 
     /**
      * Connection for this listener
@@ -70,46 +67,6 @@ public class CommandListener implements IRCListener {
     }
 
     /**
-     * Enter a parse tree produced by {@link IRCParser#commands}.
-     *
-     * @param ctx the parse tree
-     */
-    @Override
-    public void enterCommands(IRCParser.CommandsContext ctx) {
-
-    }
-
-    /**
-     * Exit a parse tree produced by {@link IRCParser#commands}.
-     *
-     * @param ctx the parse tree
-     */
-    @Override
-    public void exitCommands(IRCParser.CommandsContext ctx) {
-
-    }
-
-    /**
-     * Enter a parse tree produced by {@link IRCParser#initCommand}.
-     *
-     * @param ctx the parse tree
-     */
-    @Override
-    public void enterInitCommand(IRCParser.InitCommandContext ctx) {
-
-    }
-
-    /**
-     * Exit a parse tree produced by {@link IRCParser#initCommand}.
-     *
-     * @param ctx the parse tree
-     */
-    @Override
-    public void exitInitCommand(IRCParser.InitCommandContext ctx) {
-
-    }
-
-    /**
      * Enter a parse tree produced by {@link IRCParser#nickCommand}.
      *
      * @param ctx the parse tree
@@ -117,16 +74,6 @@ public class CommandListener implements IRCListener {
     @Override
     public void enterNickCommand(IRCParser.NickCommandContext ctx) {
         connection.tell(MessageBuilder.change(Change.Field.NICKNAME, ctx.user().getText()), worker);
-    }
-
-    /**
-     * Exit a parse tree produced by {@link IRCParser#nickCommand}.
-     *
-     * @param ctx the parse tree
-     */
-    @Override
-    public void exitNickCommand(IRCParser.NickCommandContext ctx) {
-
     }
 
     /**
@@ -140,36 +87,6 @@ public class CommandListener implements IRCListener {
     }
 
     /**
-     * Exit a parse tree produced by {@link IRCParser#userCommand}.
-     *
-     * @param ctx the parse tree
-     */
-    @Override
-    public void exitUserCommand(IRCParser.UserCommandContext ctx) {
-
-    }
-
-    /**
-     * Enter a parse tree produced by {@link IRCParser#capCommand}.
-     *
-     * @param ctx the parse tree
-     */
-    @Override
-    public void enterCapCommand(IRCParser.CapCommandContext ctx) {
-
-    }
-
-    /**
-     * Exit a parse tree produced by {@link IRCParser#capCommand}.
-     *
-     * @param ctx the parse tree
-     */
-    @Override
-    public void exitCapCommand(IRCParser.CapCommandContext ctx) {
-
-    }
-
-    /**
      * Enter a parse tree produced by {@link IRCParser#quitCommand}.
      *
      * @param ctx the parse tree
@@ -179,16 +96,6 @@ public class CommandListener implements IRCListener {
         connection.tell(MessageBuilder.logout(
                 ctx.message() == null ? Template.get("quitMessage").toString() : ctx.message().getText()
         ), worker);
-    }
-
-    /**
-     * Exit a parse tree produced by {@link IRCParser#quitCommand}.
-     *
-     * @param ctx the parse tree
-     */
-    @Override
-    public void exitQuitCommand(IRCParser.QuitCommandContext ctx) {
-
     }
 
     /**
@@ -207,16 +114,6 @@ public class CommandListener implements IRCListener {
     }
 
     /**
-     * Exit a parse tree produced by {@link IRCParser#privmsgCommand}.
-     *
-     * @param ctx the parse tree
-     */
-    @Override
-    public void exitPrivmsgCommand(IRCParser.PrivmsgCommandContext ctx) {
-
-    }
-
-    /**
      * Enter a parse tree produced by {@link IRCParser#notice}.
      *
      * @param ctx the parse tree
@@ -232,16 +129,6 @@ public class CommandListener implements IRCListener {
     }
 
     /**
-     * Exit a parse tree produced by {@link IRCParser#notice}.
-     *
-     * @param ctx the parse tree
-     */
-    @Override
-    public void exitNotice(IRCParser.NoticeContext ctx) {
-
-    }
-
-    /**
      * Enter a parse tree produced by {@link IRCParser#ping}.
      *
      * @param ctx the parse tree
@@ -251,44 +138,9 @@ public class CommandListener implements IRCListener {
         connection.tell(Template.get("pong").toString(), worker);
     }
 
-    /**
-     * Exit a parse tree produced by {@link IRCParser#ping}.
-     *
-     * @param ctx the parse tree
-     */
-    @Override
-    public void exitPing(IRCParser.PingContext ctx) {
-
-    }
-
-    /**
-     * Enter a parse tree produced by {@link IRCParser#pong}.
-     *
-     * @param ctx the parse tree
-     */
-    @Override
-    public void enterPong(IRCParser.PongContext ctx) {
-
-    }
-
-    /**
-     * Exit a parse tree produced by {@link IRCParser#pong}.
-     *
-     * @param ctx the parse tree
-     */
-    @Override
-    public void exitPong(IRCParser.PongContext ctx) {
-
-    }
-
     @Override
     public void enterMotd(IRCParser.MotdContext ctx) {
         connection.tell(Template.get("motd").toString(), worker);
-    }
-
-    @Override
-    public void exitMotd(IRCParser.MotdContext ctx) {
-
     }
 
     /**
@@ -302,36 +154,6 @@ public class CommandListener implements IRCListener {
     }
 
     /**
-     * Exit a parse tree produced by {@link IRCParser#whois}.
-     *
-     * @param ctx the parse tree
-     */
-    @Override
-    public void exitWhois(IRCParser.WhoisContext ctx) {
-
-    }
-
-    /**
-     * Enter a parse tree produced by {@link IRCParser#lusers}.
-     *
-     * @param ctx the parse tree
-     */
-    @Override
-    public void enterLusers(IRCParser.LusersContext ctx) {
-
-    }
-
-    /**
-     * Exit a parse tree produced by {@link IRCParser#lusers}.
-     *
-     * @param ctx the parse tree
-     */
-    @Override
-    public void exitLusers(IRCParser.LusersContext ctx) {
-
-    }
-
-    /**
      * Enter a parse tree produced by {@link IRCParser#join}.
      *
      * @param ctx the parse tree
@@ -340,155 +162,5 @@ public class CommandListener implements IRCListener {
     public void enterJoin(IRCParser.JoinContext ctx) {
         //connection.join(ctx.WORD().getText());/
         connection.tell(MessageBuilder.join(ctx.WORD().getText()), worker);
-    }
-
-    /**
-     * Exit a parse tree produced by {@link IRCParser#join}.
-     *
-     * @param ctx the parse tree
-     */
-    @Override
-    public void exitJoin(IRCParser.JoinContext ctx) {
-
-    }
-
-    /**
-     * Enter a parse tree produced by {@link IRCParser#realname}.
-     *
-     * @param ctx the parse tree
-     */
-    @Override
-    public void enterRealname(IRCParser.RealnameContext ctx) {
-
-    }
-
-    /**
-     * Exit a parse tree produced by {@link IRCParser#realname}.
-     *
-     * @param ctx the parse tree
-     */
-    @Override
-    public void exitRealname(IRCParser.RealnameContext ctx) {
-
-    }
-
-    /**
-     * Enter a parse tree produced by {@link IRCParser#message}.
-     *
-     * @param ctx the parse tree
-     */
-    @Override
-    public void enterMessage(IRCParser.MessageContext ctx) {
-
-    }
-
-    /**
-     * Exit a parse tree produced by {@link IRCParser#message}.
-     *
-     * @param ctx the parse tree
-     */
-    @Override
-    public void exitMessage(IRCParser.MessageContext ctx) {
-
-    }
-
-    /**
-     * Enter a parse tree produced by {@link IRCParser#user}.
-     *
-     * @param ctx the parse tree
-     */
-    @Override
-    public void enterUser(IRCParser.UserContext ctx) {
-
-    }
-
-    /**
-     * Exit a parse tree produced by {@link IRCParser#user}.
-     *
-     * @param ctx the parse tree
-     */
-    @Override
-    public void exitUser(IRCParser.UserContext ctx) {
-
-    }
-
-    /**
-     * Enter a parse tree produced by {@link IRCParser#userMode}.
-     *
-     * @param ctx the parse tree
-     */
-    @Override
-    public void enterUserMode(IRCParser.UserModeContext ctx) {
-
-    }
-
-    /**
-     * Exit a parse tree produced by {@link IRCParser#userMode}.
-     *
-     * @param ctx the parse tree
-     */
-    @Override
-    public void exitUserMode(IRCParser.UserModeContext ctx) {
-
-    }
-
-    /**
-     * Enter a parse tree produced by {@link IRCParser#unused}.
-     *
-     * @param ctx the parse tree
-     */
-    @Override
-    public void enterUnused(IRCParser.UnusedContext ctx) {
-
-    }
-
-    /**
-     * Exit a parse tree produced by {@link IRCParser#unused}.
-     *
-     * @param ctx the parse tree
-     */
-    @Override
-    public void exitUnused(IRCParser.UnusedContext ctx) {
-
-    }
-
-    /**
-     * Enter a parse tree produced by {@link IRCParser#delimiter}.
-     *
-     * @param ctx the parse tree
-     */
-    @Override
-    public void enterDelimiter(IRCParser.DelimiterContext ctx) {
-
-    }
-
-    /**
-     * Exit a parse tree produced by {@link IRCParser#delimiter}.
-     *
-     * @param ctx the parse tree
-     */
-    @Override
-    public void exitDelimiter(IRCParser.DelimiterContext ctx) {
-
-    }
-
-    @Override
-    public void visitTerminal(TerminalNode terminalNode) {
-
-    }
-
-    @Override
-    public void visitErrorNode(ErrorNode errorNode) {
-
-    }
-
-    @Override
-    public void enterEveryRule(ParserRuleContext parserRuleContext) {
-
-    }
-
-    @Override
-    public void exitEveryRule(ParserRuleContext parserRuleContext) {
-
     }
 }
